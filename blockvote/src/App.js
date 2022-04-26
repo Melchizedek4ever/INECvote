678ebf4fa3536efa15bbd80a35af6754c7df700e
Ezra Kewa <88113467+Melchizedek4ever@users.noreply.github.com>
2022-04-26 16:23:12 +0100

    initial commit

---
+++ blockvote/src/App.js
@@ -0,0 +1,11 @@
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import { login, logout } from './utils'
+import './global.css'
+
+import getConfig from './config'
+const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+
+export default function App() {
+  return <div>Hello World</div>;
+}
