48b37acc3cf6446ea14fe004a365cf4ed1967c32
Ezra Kewa <88113467+Melchizedek4ever@users.noreply.github.com>
2022-04-26 16:55:52 +0100

    initial commit

--- blockvote/src/App.js
+++ blockvote/src/App.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime/runtime'
 import React from 'react'
 import { login, logout } from './utils'
 import './global.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import getConfig from './config'
 const { networkId } = getConfig(process.env.NODE_ENV || 'development')
